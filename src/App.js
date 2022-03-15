102a3cd00a25e9733727b77e4b76902c6bc90746
Burak Seyit Kara <burakseyitkara@icloud.com>
2022-03-15 09:25:32 +0300

    Conditionals  in JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const title = "Blog post";
+  const body = "This is a blog post";
+  const comments = [
+    { id: 1, text: "Commeent 1" },
+    { id: 2, text: "Commeent 2" },
+    { id: 3, text: "Commeent 3" },
+  ];
+
+  const loading = false;
+  const showComments = true;
+
+  const commentBlock = (
+    <div className="comments">
+      <h3>Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
+  if (loading) return <h1>Loading...</h1>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{title}</h1>
+      <p> {body} </p>
+
+      {showComments && commentBlock}
     </div>
   );
 }
